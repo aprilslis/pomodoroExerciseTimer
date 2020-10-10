8baee0b67b83eda295dce174e19f44e8a28dc094
yiti_ <yitijiang@gmail.com>
2020-10-10 14:19:31 -0400

    pages fix

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -1,8 +1,14 @@
-class LoginPage extends component{
+import React, {Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export class LoginPage extends Component{
     constructor(){
         super();
     }
     render(){
-        return;
+        return(
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Login</Text>
+        </View>);
     }
 }
--- pages/settingPage.js
+++ pages/settingPage.js
@@ -1,8 +1,15 @@
-class SettingPage extends component{
+import React, {Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export class SettingPage extends Component{
     constructor(){
         super();
     }
     render(){
-        return;
+        return(
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Settings</Text>
+            </View>);
     }
 }
